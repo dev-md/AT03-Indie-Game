b48730370281f73f675a044daeaf81e9ea20fa4e
dev-md <30053790@tafe.wa.edu.au>
2022-04-22 09:07:17 +0800

    -Menu can do Scene changes, but outline gets a error from this.

--- AT03 Game/Assets/Scripts/Ui/MenuTempSCR.cs
+++ AT03 Game/Assets/Scripts/Ui/MenuTempSCR.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuTempSCR : MonoBehaviour
 {
     public void DebugMessage(string msg)
     {
         Debug.Log(msg);
+        if(msg == "Game Plane")
+        {
+            SceneManager.LoadScene("Game Plane");
+        }
     }
 }
