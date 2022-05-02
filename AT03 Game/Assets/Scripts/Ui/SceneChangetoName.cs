6e507426bf6d8ccf7a902b17b22989760c07ddd0
dev-md <30053790@tafe.wa.edu.au>
2022-05-02 10:59:24 +0800

    -Quick update, done the scene change scr

--- AT03 Game/Assets/Scripts/Ui/SceneChangetoName.cs
+++ AT03 Game/Assets/Scripts/Ui/SceneChangetoName.cs
@@ -6,24 +6,41 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangetoName : MonoBehaviour
 {
+    private List<string> scenes = new List<string>();
     private void Awake()
     {
-       List<string> scenes = new List<string>();
        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
        {
            if (scene.enabled)
             {
                 scenes.Add(scene.path);
-                print(scene.);
+                //print(scene.path);
             }
        }
+       for(int i = 0; i < scenes.Count; i++)
+        {
+            if((scenes[i].IndexOf("SCE_")) > 0)
+            {
+                scenes[i] = scenes[i].Substring(scenes[i].IndexOf("SCE_"));
+                scenes[i] = scenes[i].Replace(".unity", "");
+            }
+            else if ((scenes[i].IndexOf("SCE_")) < 0)
+            {
+                scenes[i] = null;
+            }
+        }
     }
     public void DebugMessage(string msg)
     {
         Debug.Log(msg);
-        if (msg == "Game Plane")
+        foreach(string scene in scenes)
         {
-            SceneManager.LoadScene("Game Plane");
+            //print(scene);
+            if (msg == scene)
+            {
+                SceneManager.LoadScene(scene);
+            }
         }
+        
     }
 }
