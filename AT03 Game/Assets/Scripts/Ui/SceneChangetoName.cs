d5c2711ea4bd16110d1d32360499461e4edf143f
dev-md <30053790@tafe.wa.edu.au>
2022-05-23 12:50:12 +0800

    -Build of the game.

--- AT03 Game/Assets/Scripts/Ui/SceneChangetoName.cs
+++ AT03 Game/Assets/Scripts/Ui/SceneChangetoName.cs
@@ -1,39 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChangetoName : MonoBehaviour
 {
-    private List<string> scenes = new List<string>();
+    private List<string> listscenes = new List<string>();
     private void Awake()
     {
-       foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
-       {
-           if (scene.enabled)
-            {
-                scenes.Add(scene.path);
-                //print(scene.path);
-            }
-       }
-       for(int i = 0; i < scenes.Count; i++)
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+        string[] scenes = new string[sceneCount];
+        for (int i = 0; i < sceneCount; i++)
         {
-            if((scenes[i].IndexOf("SCE_")) > 0)
-            {
-                scenes[i] = scenes[i].Substring(scenes[i].IndexOf("SCE_"));
-                scenes[i] = scenes[i].Replace(".unity", "");
-            }
-            else if ((scenes[i].IndexOf("SCE_")) < 0)
-            {
-                scenes[i] = null;
-            }
+            scenes[i] = System.IO.Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i));
+            //print(scenes[i]);
+        }
+        foreach(string s in scenes)
+        {
+            listscenes.Add(s);
         }
     }
+
     public void DebugMessage(string msg)
     {
         //Debug.Log(msg);
-        foreach(string scene in scenes)
+        foreach(string scene in listscenes)
         {
             //print(scene);
             if (msg == scene)
